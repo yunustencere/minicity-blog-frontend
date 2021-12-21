d2012b75ffae5aa20481a8057e8df29fa8894f84
yunus tencere <baytencere@gmail.com>
2021-12-21 22:39:50 +0300

    eslint installed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./views/navbar/Navbar";
 import ViewsMain from "./views/ViewsMain";
-function App() {
+
+const App = () => {
   return (
     <div className="container-fluid p-0 m-0">
       <div className="row g-0">
